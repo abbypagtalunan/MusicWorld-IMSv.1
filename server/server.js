d00e53c153211eab20535ee6f0b931680c1737b6
Jerome Kenneth <jnaguilar1@up.edu.ph>
2025-04-25 11:24:28 +0800

    server,jserror

--- server/server.js
+++ server/server.js
@@ -1,18 +1,13 @@
-const express = require("express");
-const cors = require("cors");
-const app = express();
-const supplierRoutes = require('./src/routes/supplierRoutes.js');
-const brandRoutes = require('./src/routes/brandRoutes.js');
-const categoryRoutes = require('./src/routes/categoryRoutes.js');
-const productRoutes = require('./src/routes/productRoutes.js');
-const productStatusRoutes = require('./src/routes/productStatusRoutes.js');
-const deliveryModeOfPaymentRoutes = require('./src/routes/deliveryModeOfPaymentRouter.js');
+const supplierRoutes = require('../server/src/routes/supplierRoutes.js');
+const brandRoutes = require('../server/src/routes/brandRoutes.js');
+const categoryRoutes = require('../server/src/routes/categoryRoutes.js');
+const productRoutes = require('../server/src/routes/productRoutes.js');
+const productStockRoutes = require('../server/src/routes/productStockRoutes.js');
+const productStatusRoutes = require('../server/src/routes/productStatusRoutes.js');
+const deliveryModeOfPaymentRoutes = require('../server/src/routes/deliveryModeOfPaymentRouter.js');
 const deliveryPaymentTypesRoutes = require('./src/routes/deliveryPaymentTypesRoutes.js');
 const deliveryPaymentStatusRoutes = require('./src/routes/deliveryPaymentStatusRoutes.js');
-const returnsRoutes = require('./src/routes/returnsRoutes.js');
-const supBrdCatStatusRoutes = require('./src/routes/SupBrdCatStatusRoutes.js');
-const returnTypeRoutes = require('./src/routes/returnTypeRoutes.js');
-app.use(
+const returnRoutes = require('../server/src/routes/returnsRoutes.js');
   cors({
     //allow origin port of frontend
     //change to * if all origin, pero specify muna natin
@@ -20,7 +15,7 @@ app.use(
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ['Content-Type']
   })
-)
+
 app.use(express.json());
 
 // Use all routes for each entity here
@@ -33,7 +28,6 @@ app.use("/deliveryMOP", deliveryModeOfPaymentRoutes);
 app.use("/deliveryPayTypes", deliveryPaymentTypesRoutes);
 app.use("/deliveryPayStatus", deliveryPaymentStatusRoutes);
 app.use('/returns', returnsRoutes);
-app.use('/returnType', returnsRoutes);
 app.use("/supBrdCatStatus", supBrdCatStatusRoutes);
 
 app.use(cors());
