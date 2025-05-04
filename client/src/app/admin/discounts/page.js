6924e38d0163270fc71bed472e7762e7e255b5e1
Angelica Dizon <apdizon3@up.edu.ph>
2025-05-04 20:42:36 +0800

    improved dialogs for discounts

--- client/src/app/admin/discounts/page.js
+++ client/src/app/admin/discounts/page.js
@@ -248,7 +248,7 @@ export default function DiscountsPage() {
                     Delete Selected
                   </Button>
                 </DialogTrigger>
-                <DialogContent className="max-w-3xl p-7 text-gray-700">
+                <DialogContent className="w-[90vw] max-w-md sm:max-w-lg md:max-w-xl max-h-[90vh] overflow-y-auto p-6">
                   <DialogHeader>
                     <DialogTitle>
                       <span className="text-lg text-red-900">Delete Multiple Transactions</span>
@@ -259,7 +259,7 @@ export default function DiscountsPage() {
                   <p className="text-sm text-gray-800 mt-2 pl-4">
                     Deleting these transactions will reflect on Void Transactions. Enter the admin password to delete the selected products.
                   </p>
-                  <div className="flex items-center gap-4 mt-4 pl-10">
+                  <div className="flex gap-4 mt-4 text-gray-700 items-center pl-4">
                     <div className="flex-1">
                       <label htmlFor="password" className="text-base font-medium text-gray-700 block mb-2">
                         Admin Password
@@ -332,7 +332,7 @@ export default function DiscountsPage() {
                             <Ellipsis size={16} />
                           </Button>
                         </DialogTrigger>
-                        <DialogContent className="max-w-3xl p-6">
+                        <DialogContent className="w-[90vw] max-w-3xl sm:max-w-lg md:max-w-3xl max-h-[90vh] overflow-y-auto p-6">
                         <DialogHeader>
                             <DialogTitle>Transaction Details</DialogTitle>
                             <DialogClose />
@@ -377,7 +377,7 @@ export default function DiscountsPage() {
                         <Trash2 size={16} />
                       </Button>
                         </DialogTrigger>
-                        <DialogContent className="max-w-3xl p-7 text-gray-700">
+                        <DialogContent className="w-[90vw] max-w-md sm:max-w-lg md:max-w-xl max-h-[90vh] overflow-y-auto p-6">
                         <DialogHeader>
                             <DialogTitle>
                               <span className="text-lg text-red-900">Delete Transaction</span>{" "}
@@ -385,7 +385,7 @@ export default function DiscountsPage() {
                             <DialogClose />
                           </DialogHeader>
                           <p className='text-sm text-gray-800 mt-2 pl-4'> Deleting this transaction will reflect on Void Transactions. Enter the admin password to delete this transaction. </p>
-                          <div className="flex items-center gap-4 mt-4 pl-10">          
+                          <div className="flex gap-4 mt-4 text-gray-700 items-center pl-4">        
                             <div className="flex-1">
                               <label htmlFor={`password-${transaction.transactionID}`} className="text-base font-medium text-gray-700 block mb-2">
                                 Admin Password
