8331398e35ce5622ea521726902db67601eb4a8c
abbypagtalunan <abigail.pagtalunan12@gmail.com>
2025-05-15 15:04:18 +0800

    Migrated to local database, server/db,js modified and added sql file.

--- server/db.js
+++ server/db.js
@@ -1,25 +1,53 @@
+// const mysql = require('mysql2');
+// const db = mysql.createPool({
+//   host: 'mysql-d56cb8-mworld-inventory.g.aivencloud.com',
+//   user: 'avnadmin',
+//   password: 'AVNS_zCHF-_W0nDdwgf4J8sC',
+//   database: 'defaultdb',
+//   port: 11348,
+//   ssl: {
+//     rejectUnauthorized: false
+//   },
+//   waitForConnections: true,
+//   connectionLimit: 10,  // Limit the number of connections in the pool
+//   queueLimit: 0
+// });
+
+// db.getConnection((err, connection) => {
+//     if (err) {
+//       console.error('Could not connect to database:', err);
+//     } else {
+//       console.log('Connected to MySQL database');
+//       connection.release();
+//     }
+//   });
+  
+//   module.exports = db;
+
+
+// FOR REGTEST
+
 const mysql = require('mysql2');
+
 const db = mysql.createPool({
-  host: 'mysql-d56cb8-mworld-inventory.g.aivencloud.com',
-  user: 'avnadmin',
-  password: 'AVNS_zCHF-_W0nDdwgf4J8sC',
+  host: 'localhost',
+  user: 'root',
+  password: '', // ⬅️ No password
   database: 'defaultdb',
-  port: 11348,
-  ssl: {
-    rejectUnauthorized: false
-  },
+  port: 3306,
   waitForConnections: true,
-  connectionLimit: 10,  // Limit the number of connections in the pool
+  connectionLimit: 10,
   queueLimit: 0
 });
 
 db.getConnection((err, connection) => {
-    if (err) {
-      console.error('Could not connect to database:', err);
-    } else {
-      console.log('Connected to MySQL database');
-      connection.release();
-    }
-  });
-  
-  module.exports = db;
+  if (err) {
+    console.error('Could not connect to local database:', err);
+  } else {
+    console.log('Connected to local MySQL database');
+    connection.release();
+  }
+});
+
+module.exports = db;
+
