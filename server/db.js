7d2a97208aae7fc920b43fbfb8f3b6a60115196c
abbypagtalunan <abigail.pagtalunan12@gmail.com>
2025-05-16 10:47:53 +0800

    Reverted to remote database configuration

--- server/db.js
+++ server/db.js
@@ -1,53 +1,53 @@
-// const mysql = require('mysql2');
-// const db = mysql.createPool({
-//   host: 'mysql-d56cb8-mworld-inventory.g.aivencloud.com',
-//   user: 'avnadmin',
-//   password: 'AVNS_zCHF-_W0nDdwgf4J8sC',
-//   database: 'defaultdb',
-//   port: 11348,
-//   ssl: {
-//     rejectUnauthorized: false
-//   },
-//   waitForConnections: true,
-//   connectionLimit: 10,  // Limit the number of connections in the pool
-//   queueLimit: 0
-// });
-
-// db.getConnection((err, connection) => {
-//     if (err) {
-//       console.error('Could not connect to database:', err);
-//     } else {
-//       console.log('Connected to MySQL database');
-//       connection.release();
-//     }
-//   });
-  
-//   module.exports = db;
-
-
-// FOR REGTEST
-
 const mysql = require('mysql2');
-
 const db = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '', 
+  host: 'mysql-d56cb8-mworld-inventory.g.aivencloud.com',
+  user: 'avnadmin',
+  password: 'AVNS_zCHF-_W0nDdwgf4J8sC',
   database: 'defaultdb',
-  port: 3306,
+  port: 11348,
+  ssl: {
+    rejectUnauthorized: false
+  },
   waitForConnections: true,
-  connectionLimit: 10,
+  connectionLimit: 10,  // Limit the number of connections in the pool
   queueLimit: 0
 });
 
 db.getConnection((err, connection) => {
-  if (err) {
-    console.error('Could not connect to local database:', err);
-  } else {
-    console.log('Connected to local MySQL database FOR REG TEST');
-    connection.release();
-  }
-});
+    if (err) {
+      console.error('Could not connect to database:', err);
+    } else {
+      console.log('Connected to MySQL database');
+      connection.release();
+    }
+  });
+  
+  module.exports = db;
 
-module.exports = db;
+
+// FOR REGTEST
+
+// const mysql = require('mysql2');
+
+// const db = mysql.createPool({
+//   host: 'localhost',
+//   user: 'root',
+//   password: '', 
+//   database: 'defaultdb',
+//   port: 3306,
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0
+// });
+
+// db.getConnection((err, connection) => {
+//   if (err) {
+//     console.error('Could not connect to local database:', err);
+//   } else {
+//     console.log('Connected to local MySQL database FOR REG TEST');
+//     connection.release();
+//   }
+// });
+
+// module.exports = db;
 
